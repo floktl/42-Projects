b2c2ccf41ab4ee2db4932b8b1143fcd312323107
floktl <fl.keitel@gmail.com>
2024-03-05 13:27:59 +0100

    some tests

--- 42_Projects/tests/blah/blah.c
+++ 42_Projects/tests/blah/blah.c
@@ -3,92 +3,52 @@
 /*                                                        :::      ::::::::   */
 /*   blah.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fkeitel <fkeitel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: flo <flo@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 16:34:50 by fkeitel           #+#    #+#             */
-/*   Updated: 2023/12/02 20:06:39 by fkeitel          ###   ########.fr       */
+/*   Updated: 2024/02/27 22:00:35 by flo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-# include <limits.h>
+#include <limits.h>
 
-int	find_highest(int *array, int size)
+typedef struct s_window
 {
-	int	i;
-	int	highest;
+	double	zoom;
+	int		x_mouse_posx;
+	int		y_mouse_posy;
+}	t_window;
 
-	if (size <= 0)
-		return (0);
-	highest = array[0];
-	i = 1;
-	while (i < size)
-	{
-		if (array[i] > highest)
-			highest = array[i];
-		i++;
-	}
-	return (highest);
-}
-
-int	find_position(int num, int *array, int size)
+int	zoom_calculations(t_window *window, int *xw, int *yw)
 {
-	int	i;
-
-	i = 0;
-	while (array[i] != '\0')
+	if (window->zoom != 1)
 	{
-		if (array[i] == (num + 1))
-			return (0);
-		if (array[i] == num)
-			return (i + 1);
-		i++;
+		if (*xw < window->x_mouse_posx && *yw < window->y_mouse_posy)
+		{
+			*xw -= window->zoom;
+			*yw -= window->zoom;
+		}
+		else if (*xw < window->x_mouse_posx && *yw > window->y_mouse_posy)
+		{
+			*xw *= window->zoom;
+		}
+		else if (*xw > window->x_mouse_posx && *yw < window->y_mouse_posy)
+		{
+			*yw *= window->zoom;
+		}
 	}
 	return (0);
 }
 
-int	calculate_rotations(int *array, int size, int num)
+int main()
 {
-	int	target;
-	int	pos;
-	int	rotation_counter;
+	t_window	window = {1.5, 30, 40};
+	int			xw = 5;
+	int			yw = 15;
 
-	if (num == 1)
-		target = find_highest(array, size);
-	else
-		target = num - 1;
-	pos = find_position(target, array, size) - 1;
-	if ((size % 2 == 0 && pos > (size / 2))
-		|| (size % 2 != 0 && (size - pos) <= pos))
-		pos *= -1;
-	rotation_counter = 0;
-	if (-pos == size - 1)
-		return (-1);
-	while (pos > 0)
-	{
-		rotation_counter++;
-		pos--;
-	}
-	if (pos < 0)
-		pos = (size + pos) * (-1);
-	while (pos++ < 0)
-		rotation_counter--;
-	return (rotation_counter);
-}
+	zoom_calculations(&window, &xw, &yw);
+	printf("Updated Coordinates: (%d, %d)\n", xw, yw);
 
-int	main(void)
-{
-	int	stack[] = {3, 2, 1, 6, 7, 4};
-	int	num_to_push = 5;
-	int	stack_size = sizeof(stack) / sizeof(stack[0]);
-	int	rotations_needed;
-
-	rotations_needed = calculate_rotations(stack, stack_size, num_to_push);
-	if (rotations_needed == INT_MIN)
-		return (-1);
-	if (rotations_needed >= 0)
-		printf("Perform 'rb' %d times, then 'pb'.\n", rotations_needed);
-	if (rotations_needed < 0)
-		printf("Perform 'rrb' %d times, then 'pb'.\n", rotations_needed);
 	return (0);
 }
